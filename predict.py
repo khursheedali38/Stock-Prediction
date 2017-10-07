7678e92bf33566c4109aeaa14e94a10fff50049f
Khursheed Ali <mohammedkh@maqsoftware.net>
2017-10-07 13:50:18 +0530

    pickled up classifier

--- predict.py
+++ predict.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
 style.use('ggplot')
+import pickle
 
 #get data
 df = quandl.get('WIKI/GOOGL')
@@ -37,9 +38,16 @@ y = np.array(df['label'])
 #splitting into train and test set
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
 
-#decide the classifier and fit it onto the data
-clf = LinearRegression()
-clf.fit(X_train, y_train)
+###decide the classifier and fit it onto the data
+##clf = LinearRegression()
+##clf.fit(X_train, y_train)
+##
+###saving the classifer
+##with open('linearregression.pickle', 'wb') as f:
+##    pickle.dump(clf, f)
+
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in) 
 
 #get the accuracy
 accuracy = clf.score(X_test, y_test)
