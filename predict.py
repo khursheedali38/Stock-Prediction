3909ece9acde6886f929d816c9eccc165aeb7d75
Khursheed Ali <mohammedkh@maqsoftware.net>
2017-10-07 13:10:43 +0530

    forecast & visualize predicted stock prices

--- predict.py
+++ predict.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
-import quandl, math
+import quandl, math, datetime
 from sklearn import preprocessing, svm, cross_validation
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
 
 #get data
 df = quandl.get('WIKI/GOOGL')
@@ -21,18 +24,21 @@ df.fillna(-9999, inplace = True)
 forecast_out = int(math.ceil(0.01*len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace = True)
 
 #Preprocessing or normalizing data/features
 X = np.array(df.drop(['label'], 1))
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_later = X[-forecast_out:]
+
+df.dropna(inplace = True)
 y = np.array(df['label'])
 
 #splitting into train and test set
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
 
 #decide the classifier and fit it onto the data
-clf = svm.SVR()
+clf = LinearRegression()
 clf.fit(X_train, y_train)
 
 #get the accuracy
@@ -40,3 +46,30 @@ accuracy = clf.score(X_test, y_test)
 
 #print(len(X), len(y))
 print(accuracy)
+
+#forecasting out predicted values
+forecast_set = clf.predict(X_later)
+
+print(forecast_set, accuracy, forecast_out)
+
+#visualizing data using graph
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+oneday = 86400
+next_unix = last_unix + oneday
+
+df['Forecast'] = np.nan
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix+=86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+    
+
